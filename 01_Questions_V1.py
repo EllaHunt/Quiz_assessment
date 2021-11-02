cd153770b34ba1021942c1df7adbed6a953fed27
EllaHunt <hunt.ella@awatapu.school.nz>
2021-11-02 13:26:15 +1300

    Q_V1

---
+++ 01_Questions_V1.py
@@ -0,0 +1,25 @@
+q1 = int(input("what is 4 x 8?"))
+if q1 == 32:
+    print("correct")
+else:
+    print("incorrect")
+q2 = int(input("what is 10 - 6?"))
+if q2 == 4:
+    print("correct")
+else:
+    print("incorrect")
+q3 = int(input("what is 2 x 8?"))
+if q3 == 16:
+    print("correct")
+else:
+    print("incorrect")
+q4 = int(input("what is 12 x 2?"))
+if q4 == 24:
+    print("correct")
+else:
+    print("incorrect")
+q5 = int(input("what is 8 x 8?"))
+if q5 == 64:
+    print("correct")
+else:
+    print("incorrect")
